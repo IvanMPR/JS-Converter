94744cacac8867c8621485471a2e3ec8939ac2d3
Ivan MPR <ivsanmas@gmail.com>
2021-07-15 23:28:14 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
+const milesInput = document.getElementById("miles-input");
+const kmInput = document.getElementById("km-input");
+const resetBtn = document.getElementById("clear");
+//////////////////////////////////////////////////////////////
+window.addEventListener("load", function () {
+  milesInput.focus();
+});
+
 function milesConvert() {
-  var milesInput = document.getElementById("miles-input");
   var conversionAmount = milesInput.value;
   var convertedValue = conversionAmount * 1.6;
   if (isNaN(conversionAmount)) {
     document.getElementById("km-input").value =
       " Error ! Please enter a numeric value ! ";
     document.getElementById("km-input").style.color = "red";
-  }
-  //} else if (conversionAmount.indexOf(0) === 0 && Number.isNaN(conversionAmount.indexOf(1))) {
-  //   document.getElementById("km-output").value =
-  //     " Error ! You can not start your input with '0' !"; }
-
-  else {
+  } else {
     document.getElementById("km-input").value = convertedValue + " kilometers";
     document.getElementById("km-input").style.color = "#333";
   }
-
-  // function timer() {
-  //   milesInput.value = "";
-  // }setTimeout(timer, 3000);
-    
 }
-  
-    
-  
-    
-  
-  
-
-
 
 function kmConvert() {
-  var kmInput = document.getElementById("km-input");
   var conversionAmount = kmInput.value;
   var convertedValue = conversionAmount / 1.6;
   if (isNaN(conversionAmount)) {
@@ -44,5 +32,6 @@ function kmConvert() {
   }
 }
 // Button function
-
-
+clear.addEventListener("click", function () {
+  document.querySelectorAll("input").forEach((input) => (input.value = ""));
+});
